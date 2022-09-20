029c32a8302631696432ad864ae9268e93935ddb
tarekmn <nytarek@gmail.com>
2022-09-20 17:11:38 -0500

    looped through questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,93 @@
 //Global variables
-var count = 20;
-var i = 0;   //question #0
-var correctAnswers = 0;
-var wrongAnswers = 0;
-
-var questions = [
-  {q: "what do you like to eat",
-   a: "steak" },
-  {q: "favorite color",
-   a: "blue" }
-]
-
-console.log(questions[0].q);
-console.log(questions[0].a);
-
-
-
+var count = 120;
+var score = 0;
+var i= 0;
 
 //Selecting elments
 var startButton = document.querySelector("#startbutton");
 var timer = document.querySelector("#timer");
-var h1 = document.querySelector("#h1");
-var p1 = document.querySelector("#p1");
+var header = document.querySelector("header");
+var main = document.querySelector("main");
+var scoreEl = document.querySelector("#scoreid");
+//var h1 = document.querySelector("#h1");
+//var p1 = document.querySelector("#p1");
+
+
+//Questions in an array of objects
+var questions = [
+  {q: "what do you like to eat?",
+  lista: ["steak", "apple", "burger", "beer"],
+   a: "steak" },
+  {q: "favorite color?",
+  lista: ["red", "orange", "blue", "green"],
+   a: "blue" }
+]
+
+console.log(questions[0].q);
+console.log(questions[0].lista[0]);
+console.log(questions[0].a);
 
 
 
+// //var buttons = document.querySelectorAll("button"); array
+//correctButton.setAttribute("data-correct", "yes");
+//key thing is we can set attribute on any DOM element
 
-// //Question #1
-// var question = document.createElement("p");
-// question.textContent = "this was made by js, yes or no?" //text
-// document.body.appendChild(question);
+function askingQuestionX () {
 
-// console.log(question);
+ //for (var i = 0; i < questions.length; i++){
+  var currentquestion = questions[i];
+  //var section = document.createElement("sections")
+  var questionText = document.createElement("h2");
+  var answerTextUl = document.createElement("ul");
+  var answerTextLi1 = document.createElement("button");
+  var answerTextLi2 = document.createElement("button");
+  var answerTextLi3 = document.createElement("button");
+  var answerTextLi4 = document.createElement("button");
 
-//Question #2 will try to replace current question/new approach
+  questionText.textContent = questions[i].q;
+  answerTextLi1.textContent = questions[i].lista[0];
+  answerTextLi2.textContent = questions[i].lista[1];
+  answerTextLi3.textContent = questions[i].lista[2];
+  answerTextLi4.textContent = questions[i].lista[3];
 
-function askingQuestion(i) {
-h1.textContent = questions[i].q;
-p1.textContent = questions[i].a;
+  main.appendChild(questionText);
+  main.appendChild(answerTextUl);
+  answerTextUl.appendChild(answerTextLi1);
+  answerTextUl.appendChild(answerTextLi2);
+  answerTextUl.appendChild(answerTextLi3);
+  answerTextUl.appendChild(answerTextLi4);
+
+  answerTextUl.addEventListener("click", function (event){
+    console.log(event.target);
+    // console.log(questions[i].a);
+    if(event.target.textContent === questions[i].a) {
+      console.log("correct answer")
+      score++ ;
+      console.log(score);
+      scoreEl.textContent = score;
+
+    } else {
+      count -= 10;
+    }
+    i++;
+    main.innerHTML = "";
+
+    if(i > questions.length - 1) {
+      //end quiz
+      //Text thats thanks for playing
+      //Display score and save initials, save to local upon reload
+    } else { 
+      askingQuestionX();
+    }
+
+   });
+
 }
 
-
-
-
-// function presentAnswers() {
-// var answer1 = document.createElement("button");
-// answer1.textContent = "Answer1";
-// document.body.appendChild(answer1);
-// var answer2 = document.createElement("button");
-// answer2.textContent = "Answer2";
-// document.body.appendChild(answer2);
-// var answer3 = document.createElement("button");
-// answer3.textContent = "Answer3";
-// document.body.appendChild(answer3);
-// var answer4 = document.createElement("button");
-// answer4.textContent = "Answer4";
-// document.body.appendChild(answer4);
+// for (var i = 0; i <  buttons.length; i++){
+// buttons[i].addEventListener("click", function(event){}
+//console.log("click"))
 // }
 
 
@@ -63,9 +95,9 @@ p1.textContent = questions[i].a;
 
 //Timer intervals Updates count on page
 function setTime() {
-  var timerInterval = setInterval(function () {
+  var timerInterval = setInterval(function() {
     count-- ;
-
+    console.log(count);
     if(count === 0) {
       clearInterval(timerInterval);
     }
@@ -78,19 +110,14 @@ function setTime() {
 //Start button
     //When clicked: 
 startButton.addEventListener("click", function (){
+  header.innerHTML = " "                   //important to clear, innerHTML is a property
   setTime(); //a timer starts 
   //question1();
-  askingQuestion(i);
-//randomize answer [array of possible answers?]
+  //askingQuestion(i);
+  //i++
 
-//if(correct button) {
-  //askingQuestion(question2title, question2)
-//};
+  askingQuestionX()
 
-  //if (correct answer) {
-  // askingQuestion(++, ++) q2 basically
-  // return correct +1 
-  //else(not correct) {timer lose 10 seconds; askingQuestions again} return wrong +1
 })
     //a timer starts 
     //presented with a question Q1
@@ -98,3 +125,6 @@ startButton.addEventListener("click", function (){
     //presented with question 2
 
 //
+
+//even listeners for when user selects option
+//if correct answer etc etc
