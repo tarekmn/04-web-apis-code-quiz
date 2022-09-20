76e9ab11bac23f2e079e0c54382cab5089cc17b3
tarekmn <nytarek@gmail.com>
2022-09-20 10:01:02 -0500

    changing questions to an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
 //Global variables
 var count = 20;
+var i = 0;   //question #0
 var correctAnswers = 0;
 var wrongAnswers = 0;
 
+var questions = [
+  {q: "what do you like to eat",
+   a: "steak" },
+  {q: "favorite color",
+   a: "blue" }
+]
 
-var question1title = "Question #1";
-var question1 = "what do you like to eat";
-var question2title ="Question #2";
-var question2 = "what is your fav color";
+console.log(questions[0].q);
+console.log(questions[0].a);
+
 
 
 
@@ -29,27 +35,28 @@ var p1 = document.querySelector("#p1");
 
 //Question #2 will try to replace current question/new approach
 
-function askingQuestion(questionNum, question) {
-h1.textContent = questionNum;
-p1.textContent = question;
-
-
+function askingQuestion(i) {
+h1.textContent = questions[i].q;
+p1.textContent = questions[i].a;
 }
 
-function presentAnswers() {
-var answer1 = document.createElement("button");
-answer1.textContent = "Answer1";
-document.body.appendChild(answer1);
-var answer2 = document.createElement("button");
-answer2.textContent = "Answer2";
-document.body.appendChild(answer2);
-var answer3 = document.createElement("button");
-answer3.textContent = "Answer3";
-document.body.appendChild(answer3);
-var answer4 = document.createElement("button");
-answer4.textContent = "Answer4";
-document.body.appendChild(answer4);
-}
+
+
+
+// function presentAnswers() {
+// var answer1 = document.createElement("button");
+// answer1.textContent = "Answer1";
+// document.body.appendChild(answer1);
+// var answer2 = document.createElement("button");
+// answer2.textContent = "Answer2";
+// document.body.appendChild(answer2);
+// var answer3 = document.createElement("button");
+// answer3.textContent = "Answer3";
+// document.body.appendChild(answer3);
+// var answer4 = document.createElement("button");
+// answer4.textContent = "Answer4";
+// document.body.appendChild(answer4);
+// }
 
 
 
@@ -73,9 +80,9 @@ function setTime() {
 startButton.addEventListener("click", function (){
   setTime(); //a timer starts 
   //question1();
-  askingQuestion(question1title, question1);
-  //present answers();
-  presentAnswers();
+  askingQuestion(i);
+//randomize answer [array of possible answers?]
+
 //if(correct button) {
   //askingQuestion(question2title, question2)
 //};
@@ -84,10 +91,6 @@ startButton.addEventListener("click", function (){
   // askingQuestion(++, ++) q2 basically
   // return correct +1 
   //else(not correct) {timer lose 10 seconds; askingQuestions again} return wrong +1
-
-
-  
-  //presented with a question Q1
 })
     //a timer starts 
     //presented with a question Q1
