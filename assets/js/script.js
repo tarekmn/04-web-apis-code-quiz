9d775c4891b46546f0732d879668b6dfe18b8328
tarekmn <nytarek@gmail.com>
2022-09-19 19:36:07 -0500

    reformating answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 //Global variables
 var count = 20;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+
+
 var question1title = "Question #1";
 var question1 = "what do you like to eat";
 var question2title ="Question #2";
 var question2 = "what is your fav color";
 
-var answer1 = "default";
 
 
 //Selecting elments
@@ -29,20 +32,23 @@ var p1 = document.querySelector("#p1");
 function askingQuestion(questionNum, question) {
 h1.textContent = questionNum;
 p1.textContent = question;
-presentAnswers();
+
 
 }
 
 function presentAnswers() {
 var answer1 = document.createElement("button");
+answer1.textContent = "Answer1";
 document.body.appendChild(answer1);
 var answer2 = document.createElement("button");
+answer2.textContent = "Answer2";
 document.body.appendChild(answer2);
 var answer3 = document.createElement("button");
+answer3.textContent = "Answer3";
 document.body.appendChild(answer3);
 var answer4 = document.createElement("button");
+answer4.textContent = "Answer4";
 document.body.appendChild(answer4);
-
 }
 
 
@@ -69,6 +75,15 @@ startButton.addEventListener("click", function (){
   //question1();
   askingQuestion(question1title, question1);
   //present answers();
+  presentAnswers();
+//if(correct button) {
+  //askingQuestion(question2title, question2)
+//};
+
+  //if (correct answer) {
+  // askingQuestion(++, ++) q2 basically
+  // return correct +1 
+  //else(not correct) {timer lose 10 seconds; askingQuestions again} return wrong +1
 
 
   
