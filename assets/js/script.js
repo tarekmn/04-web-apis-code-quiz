3f92c31ed4c753d087cde6ea9651e4248e7d58c4
tarekmn <nytarek@gmail.com>
2022-09-21 15:18:24 -0500

    created end game function, saving to local mem

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 var count = 10;
 var score = 0;
 var i= 0;
+var highScoreArray = [];   //need to retrieve from local memory and push to array. Not working
+console.log(highScoreArray);
+
 
 //Selecting elments
 var startButton = document.querySelector("#startbutton");
@@ -15,15 +18,24 @@ var scoreEl = document.querySelector("#scoreid");
 
 //Questions in an array of objects
 var questions = [
-  {q: "what do you like to eat?",
-  lista: ["steak", "apple", "burger", "beer"],
-   a: "steak" },
-  {q: "favorite color?",
-  lista: ["red", "orange", "blue", "green"],
-   a: "blue" },
-   {q: "favorite car",
-  lista: ["accord", "lambo", "escalade", "minivan"],
-   a: "lambo" },
+  {q: "Inside which HTML element do we put the JavaScript?",
+  lista: ["<javascript>", "<script>", "<js>", "<scripting>"],
+   a: "<script>" },
+  {q: "Which of the following object is the main entry point to all client-side JavaScript features and APIs? color?",
+  lista: ["position", "window", "standard", "location"],
+   a: "window" },
+   {q: "Which of the following can be used to call a JavaScript Code Snippet?",
+  lista: ["function", "preprocessor", "triggeringevent", "rmi"],
+   a: "function" },
+   {q: "Which of the following scoping type does JavaScript use?",
+  lista: ["sequential", "segmental", "lexical", "literal"],
+   a: "lexical" },
+   {q: "What is the result of console.log(p)?",
+  lista: ["valueNotfound", "referenceError", "null", "zero"],
+   a: "referenceError" },
+   {q: "Which of the following is not a framework?",
+   lista: ["JavaScript.NET", "JavaScript", "CocoaJS", "jQuery"],
+    a: "JavaScript" },
 
 ]
 
@@ -32,7 +44,36 @@ var questions = [
 //console.log(questions[0].lista[0]);
 //console.log(questions[0].a);
 
+function endGame() {
+  header.innerHTML = "" ;
+  main.innerHTML = "";
+  main.textContent= "Game Over! Please save your intials"
 
+  var inputText = document.createElement("input");
+  var inputButton = document.createElement("button");
+  //var inputLabel = document.createElement("label");
+  //inputLabel.textContent = "Your initials";
+  inputButton.textContent = "Save"
+  main.appendChild(inputText);
+  main.appendChild(inputButton);
+ // main.appendChild(inputLabel);
+
+
+inputButton.addEventListener("click", function(event){
+  //retrieve what is input text
+  console.log(inputText.value);
+  //highScoreArray.push(inputText.value);
+  localStorage.setItem("initals", inputText.value);
+  localStorage.setItem("highScore", score);
+
+  //push that and score to local memory
+})
+
+   //end quiz or basically clear everything
+      //Text thats thanks for playing
+      //Display score and save initials, save to local upon reload
+  
+}
 
 
 //function that asks questions[i]
@@ -75,10 +116,9 @@ function askingQuestionX () {
     i++;
     main.innerHTML = "";
 
-    if(i > questions.length - 1 || count === 0)  {
-      //end quiz or basically clear everything
-      //Text thats thanks for playing
-      //Display score and save initials, save to local upon reload
+    if(i > questions.length - 1)  {
+
+      endGame() 
     } else { 
       askingQuestionX();
     }
@@ -98,10 +138,17 @@ function setTime() {
     if(count === 0 || count < 0) {
       clearInterval(timerInterval);
       count = 0;
+      endGame();
     }
     timer.textContent = count;
 
   }, 1000);
+}
+
+//Retrieve high score from local memory and push into array
+function retrieveHighScore() {
+  var localHighScore = localStorage.getItem("highScore");
+  highScoreArray.push(localHighScore);
 }
 
 
@@ -110,6 +157,8 @@ function setTime() {
 startButton.addEventListener("click", function (){
   header.innerHTML = " "                   //important to clear, innerHTML is a property
   setTime(); //a timer starts 
-  askingQuestionX() 
+  askingQuestionX() ;
+  retrieveHighScore() ; //Not pushing to the array
+  
 
 });
